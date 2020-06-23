5f0cd617032fc5c9125f74f9a589e1b00d1d8029
vinayak <vinayak.sohal007@gmail.com>
2020-06-23 16:36:15 -0400

    Class ApplicantResumeRepository implemented. Unit test passed

--- CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantResumeRepository.cs
@@ -2,6 +2,8 @@
 using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -12,8 +14,34 @@ namespace CareerCloud.ADODataAccessLayer
     {
         public void Add(params ApplicantResumePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(BaseAdo.connectionString);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            conn.Open();
+            foreach (ApplicantResumePoco poco in items)
+            {
+                cmd.CommandText = @"INSERT INTO [dbo].[Applicant_Resumes]
+                       ([Id]
+                       ,[Applicant]
+                       ,[Resume]
+                       ,[Last_Updated])
+                 VALUES
+                       (@Id
+                       ,@Applicant
+                       ,@Resume
+                       ,@Last_Updated)";
+
+                cmd.Parameters.AddWithValue("@Id", poco.Id);
+                cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                cmd.Parameters.AddWithValue("@Resume", poco.Resume);
+                cmd.Parameters.AddWithValue("@Last_Updated", poco.LastUpdated);
+
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
         }
+    
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
         {
@@ -22,7 +50,27 @@ namespace CareerCloud.ADODataAccessLayer
 
         public IList<ApplicantResumePoco> GetAll(params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(BaseAdo.connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = @"SELECT * FROM Applicant_Resumes";
+            conn.Open();
+
+            int x = 0;
+            SqlDataReader rdr = cmd.ExecuteReader();
+            ApplicantResumePoco[] pocos = new ApplicantResumePoco[1000];
+            while (rdr.Read())
+                {
+                    ApplicantResumePoco poco = new ApplicantResumePoco();
+                    poco.Id = rdr.GetGuid(0);
+                    poco.Applicant = rdr.GetGuid(1);
+                    poco.Resume = rdr.GetString(2);
+                    poco.LastUpdated = rdr.IsDBNull(3) ? null : (DateTime?)rdr.GetDateTime(3);
+                    pocos[x] = poco;
+                    x++;
+                }
+                conn.Close();
+                return pocos;
         }
 
         public IList<ApplicantResumePoco> GetList(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
@@ -32,17 +80,59 @@ namespace CareerCloud.ADODataAccessLayer
 
         public ApplicantResumePoco GetSingle(Expression<Func<ApplicantResumePoco, bool>> where, params Expression<Func<ApplicantResumePoco, object>>[] navigationProperties)
         {
-            throw new NotImplementedException();
+            IQueryable<ApplicantResumePoco> pocos = GetAll().AsQueryable();
+            try
+            {
+                return pocos.Where(where).FirstOrDefault();
+
+            }
+            catch (NullReferenceException e)
+            {
+                //Exception thrown as there is null reference for after performing remove operation on Id. So no Poco exists for that Id
+                Console.WriteLine(e);
+                return null;
+            }
         }
 
         public void Remove(params ApplicantResumePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(BaseAdo.connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            foreach (ApplicantResumePoco poco in items)
+            {
+                cmd.CommandText = @"DELETE FROM [dbo].[Applicant_Resumes]
+                WHERE Id=@Id";
+                cmd.Parameters.AddWithValue("@Id", poco.Id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
         }
 
         public void Update(params ApplicantResumePoco[] items)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(BaseAdo.connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            conn.Open();
+            foreach (ApplicantResumePoco poco in items)
+            {
+                cmd.CommandText = @"UPDATE [dbo].[Applicant_Resumes]
+                   SET [Id] = @Id
+                      ,[Applicant] = @Applicant
+                      ,[Resume] = @Resume
+                      ,[Last_Updated] = @Last_Updated
+                 WHERE Id=@Id";
+
+                cmd.Parameters.AddWithValue("@Id", poco.Id);
+                cmd.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                cmd.Parameters.AddWithValue("@Resume", poco.Resume);
+                cmd.Parameters.AddWithValue("@Last_Updated", poco.LastUpdated);
+               
+                cmd.ExecuteNonQuery();
+            }
+            conn.Close();
         }
     }
 }
